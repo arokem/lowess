51931c9093fc1dbe6966ee8a65a8da9001ba31c8
arokem <arokem@gmail.com>
2015-04-08 15:48:37 -0700

    Build setup infrastructure.

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from distutils.core import setup
+
+# Get version and release info, which is all stored in lowess/version.py
+ver_file = os.path.join('lowess', 'version.py')
+with open(ver_file) as f:
+    exec(f.read())
+
+opts = dict(name=NAME,
+            maintainer=MAINTAINER,
+            maintainer_email=MAINTAINER_EMAIL,
+            description=DESCRIPTION,
+            long_description=LONG_DESCRIPTION,
+            url=URL,
+            download_url=DOWNLOAD_URL,
+            license=LICENSE,
+            classifiers=CLASSIFIERS,
+            author=AUTHOR,
+            author_email=AUTHOR_EMAIL,
+            platforms=PLATFORMS,
+            version=VERSION,
+            packages=PACKAGES,
+            # Lowess doesn't have package data:
+            #package_data=PACKAGE_DATA,
+            requires=REQUIRES)
+
+
+if __name__ == '__main__':
+    setup(**opts)
